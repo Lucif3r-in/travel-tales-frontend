5446f4acc05d6c8e7aac55671212b82c1257b4c5
Lucif3r-in <ashutosh123rath@gmail.com>
2023-07-29 08:16:08 +0530

    feat:link backend to frontend

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mysterious-boa-trunks.cyclic.app",
+  baseURL: "https://cooperative-mite-bracelet.cyclic.app",
   // baseURL: "http://localhost:5000",
 });
 
