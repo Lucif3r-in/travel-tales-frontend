a019ed35f8e60a71d51504689af02092f4f24083
Lucif3r-in <ashutosh123rath@gmail.com>
2023-08-03 22:54:19 +0530

    feat: add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import useStyles from "../Navbar/styles";
+
+const defaultTheme = createTheme();
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          backgroundColor: "transparent",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(2.5px)",
+          "-webkit-backdrop-filter": "blur(2.5px)",
+          position: "fixed",
+          left: 0,
+          bottom: 0,
+          width: "100vw",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          color: "white",
+          margin: "0 auto", // Center the container horizontally
+        }}
+      >
+        <Link to="/" className={classes.brandContainer}>
+          <img
+            src="https://res.cloudinary.com/dhnkuonev/image/upload/v1690723588/Logo_tor6pg.png"
+            alt="Travel Tales Logo"
+            className={classes.logo}
+            height={40}
+          />
+        </Link>
+        <Typography variant="body1">Made with :love: by vasudev🪶</Typography>
+        <Box>
+          <Link
+            href="https://github.com/Lucif3r-in"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon sx={{ color: "white", marginRight: 2 }} />
+          </Link>
+          <Link
+            href="mailto:ashutosh123rath@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <EmailIcon sx={{ color: "white", marginRight: 2 }} />
+          </Link>
+          <Link
+            href="https://twitter.com/Ashutos48101482"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TwitterIcon sx={{ color: "white" }} />
+          </Link>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
